acb9fb47f186d4a3fbeec8f7e5ea563fcca47eb3
Alex Stroia <salexandru995@gmail.com>
2019-04-14 07:59:29 -0700

    Added view pager for the product activity

--- app/src/main/java/co/alexdev/winy/feature/ui/product/ProductActivity.java
+++ app/src/main/java/co/alexdev/winy/feature/ui/product/ProductActivity.java
@@ -1,15 +1,21 @@
 package co.alexdev.winy.feature.ui.product;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import co.alexdev.winy.R;
+import co.alexdev.winy.databinding.ActivityProductBinding;
 
 import android.os.Bundle;
 
 public class ProductActivity extends AppCompatActivity {
 
+    private ActivityProductBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_product);
+
+        mBinding.tabLayout.setupWithViewPager(mBinding.viewPager);
     }
 }
