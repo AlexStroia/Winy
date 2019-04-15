49b42e6b064802061a42a01f4a90902d960a9cca
Alex Stroia <salexandru995@gmail.com>
2019-04-15 09:20:12 -0700

    Added packages for some other activities

---
+++ app/src/main/java/co/alexdev/winy/feature/ui/account/AccountActivity.java
@@ -0,0 +1,15 @@
+package co.alexdev.winy.feature.ui.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+import co.alexdev.winy.R;
+
+import android.os.Bundle;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
---
+++ app/src/main/java/co/alexdev/winy/feature/ui/settings/SettingsActivity.java
@@ -0,0 +1,15 @@
+package co.alexdev.winy.feature.ui.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import co.alexdev.winy.R;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
