736074471105014689e8ba2d6ae071da4085e592
Alex Stroia <salexandru995@gmail.com>
2019-04-20 07:49:48 -0700

    - Added slide transition for ActivityLogin

--- app/src/main/java/co/alexdev/winy/feature/ui/login/ActivityLogin.java
+++ app/src/main/java/co/alexdev/winy/feature/ui/login/ActivityLogin.java
@@ -8,11 +8,17 @@ import co.alexdev.winy.core.util.Constants;
 import co.alexdev.winy.databinding.ActivityLoginBinding;
 import co.alexdev.winy.feature.ui.login.uimodel.ActivityLoginViewModel;
 import co.alexdev.winy.feature.ui.product.ProductActivity;
+import co.alexdev.winy.feature.ui.signup.SignupActivity;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -25,6 +31,7 @@ public class ActivityLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        setAnimation();
         binding.setLifecycleOwner(this);
         activityLoginViewModel = ViewModelProviders.of(this).get(ActivityLoginViewModel.class);
         binding.setViewModel(activityLoginViewModel);
@@ -41,7 +48,17 @@ public class ActivityLogin extends AppCompatActivity {
         });
     }
 
+
+    private void setAnimation() {
+        Slide slideAnim = new Slide(Gravity.RIGHT);
+        slideAnim.setDuration(Constants.AnimDurations.SLIDE_ANIM);
+        slideAnim.setInterpolator(new AccelerateInterpolator());
+        getWindow().setEnterTransition(slideAnim);
+        getWindow().setExitTransition(slideAnim);
+    }
+
     public static void startActivity(Context context) {
-        context.startActivity(new Intent(context, ActivityLogin.class));
+        context.startActivity(new Intent(context, ActivityLogin.class),
+                ActivityOptions.makeSceneTransitionAnimation((Activity) context).toBundle());
     }
 }
