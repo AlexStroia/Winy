91f861d036c3d0d2aa3d9bd43b60a3f17a0b2dc1
Alexandru Stroia <alexandru.stroia@halcyonmobile.com>
2019-04-25 12:17:15 +0300

    - Added binding layout for fragment_wine

--- app/src/main/java/co/alexdev/winy/feature/ui/product/wine/WineFragment.java
+++ app/src/main/java/co/alexdev/winy/feature/ui/product/wine/WineFragment.java
@@ -3,25 +3,27 @@ package co.alexdev.winy.feature.ui.product.wine;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
-import co.alexdev.winy.BuildConfig;
 import co.alexdev.winy.R;
+import co.alexdev.winy.databinding.FragmentWineBinding;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class WineFragment extends Fragment {
+
+    private FragmentWineBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wine, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_wine, container, false);
+        binding.setLifecycleOwner(this);
+
+        return binding.getRoot();
     }
 }
