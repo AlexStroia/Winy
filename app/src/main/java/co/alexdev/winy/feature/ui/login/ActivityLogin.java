3f8e09a3acbae85bb14cebca8d3c731cf42bbfae
Alex Stroia <salexandru995@gmail.com>
2019-04-27 23:27:53 -0700

    - Added layout for the wine list item

--- app/src/main/java/co/alexdev/winy/feature/ui/login/ActivityLogin.java
+++ app/src/main/java/co/alexdev/winy/feature/ui/login/ActivityLogin.java
@@ -1,8 +1,18 @@
 package co.alexdev.winy.feature.ui.login;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import javax.inject.Inject;
+
 import co.alexdev.winy.R;
 import co.alexdev.winy.core.di.DaggerWinyComponent;
 import co.alexdev.winy.core.di.WinyComponent;
@@ -11,20 +21,6 @@ import co.alexdev.winy.core.util.ViewModelFactory;
 import co.alexdev.winy.databinding.ActivityLoginBinding;
 import co.alexdev.winy.feature.ui.login.uimodel.ActivityLoginViewModel;
 import co.alexdev.winy.feature.ui.product.ProductActivity;
-
-import android.app.Activity;
-import android.app.ActivityOptions;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.transition.Slide;
-import android.view.Gravity;
-import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import javax.inject.Inject;
 
 public class ActivityLogin extends AppCompatActivity {
 
@@ -46,7 +42,7 @@ public class ActivityLogin extends AppCompatActivity {
         binding.setViewModel(activityLoginViewModel);
 
         activityLoginViewModel.loginStateEnumLiveData.observe(this, loggedState -> {
-            if (Constants.FIREBASE_DATABASE.LOGIN_STATE.SUCCES.equals(loggedState)) {
+            if (Constants.FIREBASE_DATABASE.LOGIN_STATE.SUCCESS.equals(loggedState)) {
                 ProductActivity.startActivity(this);
             } else if (Constants.FIREBASE_DATABASE.LOGIN_STATE.LOADING.equals(loggedState)) {
                 binding.progressBar.setVisibility(View.VISIBLE);
