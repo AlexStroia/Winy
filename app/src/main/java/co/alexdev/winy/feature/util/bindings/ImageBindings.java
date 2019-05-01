e7e9a69b5d2427bb27a8f64775ee7d2942577b7d
Alex Stroia <salexandru995@gmail.com>
2019-05-01 09:59:44 -0700

    - Added progress indicator to show the loading state of the image

--- app/src/main/java/co/alexdev/winy/feature/util/bindings/ImageBindings.java
+++ app/src/main/java/co/alexdev/winy/feature/util/bindings/ImageBindings.java
@@ -2,10 +2,13 @@ package co.alexdev.winy.feature.util.bindings;
 
 import android.net.Uri;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.databinding.BindingAdapter;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import co.alexdev.winy.R;
@@ -13,15 +16,26 @@ import co.alexdev.winy.R;
 
 public class ImageBindings {
 
-    @BindingAdapter({"imageUrl"})
-    public static void setImage(ImageView image, String url) {
+    @BindingAdapter(value = {"imageUrl", "loadingIndicator"}, requireAll = false)
+    public static void setImage(ImageView image, String url, ProgressBar progressBar) {
+        progressBar.setVisibility(View.VISIBLE);
         if (!TextUtils.isEmpty(url)) {
             Picasso.get()
                     .load(Uri.parse(url))
                     .placeholder(R.drawable.ic_wine_placeholder)
                     .error(R.drawable.ic_wine_placeholder)
                     .resize(150, 300)
-                    .into(image);
+                    .into(image, new Callback() {
+                        @Override
+                        public void onSuccess() {
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onError(Exception e) {
+
+                        }
+                    });
         }
     }
 }
